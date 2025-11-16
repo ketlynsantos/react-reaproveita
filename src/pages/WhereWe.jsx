88796b26dedae3cf0f5bea259c2b7f16ba9cdabe
Ketlyn Santos <ketlyn.santos@newtesc.com.br>
2025-11-16 15:40:28 -0300

    feat: adiciona seção timeline na página onde estamos

--- src/pages/WhereWe.jsx
+++ src/pages/WhereWe.jsx
@@ -1,38 +1,95 @@
 import { useNavigate } from "react-router-dom"
+import "../styles/where-we.css"
 
 export default function WhereWe() {
     const navigate = useNavigate()
     return (
         <>
             { /* Seção Hero */ }
-            <section class="hero pd-20">
+            <section className="hero pd-20">
                 <div className="container two-column">
-                    <div class="col col-text">
-                        <h1 class="title wd-80">Onde já estamos fazendo a diferença?</h1>
-                        <p class="description wd-80">
-                            Veja onde o ReAproveita já está ajudando a reduzir o desperdício.
+                    <div className="col col-text">
+                        <h1 className="title wd-80">Onde já estamos fazendo a diferença?</h1>
+                        <p className="description wd-80">
+                            Veja onde o ReAproveita já está ajudando a reduzir o desperdício 
+                            e onde chegaremos em breve.
                         </p>
+
+                        <ul className="states-legend">
+                            <li>
+                                <span className="dot active"></span>
+                                <p>Atuação atual</p>
+                            </li>
+                            <li>
+                                <span className="dot soon"></span>
+                                <p>Em breve</p>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="col col-img">
-                        <img src="/assets/images/map.png" alt="Mapa do Brasil com o estado de SP pintado" />
+                    <div className="col col-img">
+                        <img src="/assets/images/map.svg" alt="Mapa do Brasil interativo" />
+                    </div>
+                </div>
+            </section>
+
+            { /* Seção Timeline */}
+            <section className="timeline-section pd-20">
+                <div className="container two-row">
+                    <h1 className="title wd-100">Nossa jornada</h1>
+
+                    <div className="timeline-horizontal">
+
+                        <div className="timeline-step">
+                            <div className="icon-wrapper">
+                                <p className="date">2023</p>
+                            </div>
+
+                            <div className="timeline-card">
+                                <h3>Início</h3>
+                                <p>Início das operações em São Paulo.</p>
+                            </div>
+                        </div>
+
+                        <div className="timeline-step">
+                            <div className="icon-wrapper">
+                                <p className="date">2024</p>
+                            </div>
+
+                            <div className="timeline-card">
+                                <h3>Expansão</h3>
+                                <p>Primeiros parceiros e expansão regional.</p>
+                            </div>
+                        </div>
+
+                        <div className="timeline-step">
+                            <div className="icon-wrapper">
+                                <p className="date">2025</p>
+                            </div>
+
+                            <div className="timeline-card">
+                                <h3>Novos Estados</h3>
+                                <p>Planejada expansão para RJ e MG.</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
 
             { /* Seção Estado */}
-            <section class="state pd-20">
+            <section className="state pd-20">
                 <div className="container two-column">
-                    <div class="col col-img">
+                    <div className="col col-img">
                         <img src="/assets/images/market.png" alt="Barraca de venda de alimentos" />
                     </div>
 
-                    <div class="col col-text">
-                        <h2 class="title wd-80">Quer levar o ReAproveita para o seu estado?</h2>
-                        <p class="description wd-80">
+                    <div className="col col-text">
+                        <h2 className="title wd-80">Quer levar o ReAproveita para o seu estado?</h2>
+                        <p className="description wd-80">
                             Entre em contato com a gente e vamos juntos combater o desperdício.
                         </p>
                         <button
-                            class="btn green big"
+                            className="btn green big"
                             type="button"
                             onClick={() => navigate('/fale-conosco')}
                         >
